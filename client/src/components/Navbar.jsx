8e3702837e6b95ba5679fe81ad5e9ea4ac02b06e
astro0725 <angelica.strong0725@gmail.com>
2024-03-26 13:19:11 -0700

    rm contact

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,33 +2,29 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-hsla-240-1-17-75 backdrop-blur-10 border border-jet rounded-t-lg shadow-2 z-5">
-    <ul className="flex flex-wrap justify-center items-center px-10">
-      <li className="navbar-item">
-        <NavLink to="/" className="text-white text-sm text-fs-8 px-7 py-20 transition-colors duration-300 hover:text-tertiary focus:text-tertiary active:text-primary" data-nav-link>
-          About
-        </NavLink>
-      </li>
+    <div className="items-center text-white">
+      <nav className="bg-base p-4 text-gray-300 rounded-lg">
+      <ul className="flex flex-wrap justify-center items-center px-10">
+        <li>
+          <NavLink to="/" className="text-white text-sm px-7 py-10 transition-colors duration-300 hover:text-tertiary focus:text-primary active:text-primary" data-nav-link>
+            About
+          </NavLink>
+        </li>
 
-      <li className="navbar-item">
-        <NavLink to="/resume" className="text-white text-sm text-fs-8 px-7 py-20 transition-colors duration-300 hover:text-tertiary focus:text-tertiary active:text-primary" data-nav-link>
-          Resume
-        </NavLink>
-      </li>
+        <li>
+          <NavLink to="/resume" className="text-white text-sm px-7 py-10 transition-colors duration-300 hover:text-tertiary focus:text-primary active:text-primary" data-nav-link>
+            Resume
+          </NavLink>
+        </li>
 
-      <li className="navbar-item">
-        <NavLink to="/portfolio" className="text-white text-sm text-fs-8 px-7 py-20 transition-colors duration-300 hover:text-tertiary focus:text-tertiary active:text-primary" data-nav-link>
-          Portfolio
-        </NavLink>
-      </li>
-
-      <li className="navbar-item">
-        <NavLink to="/contact" className="text-white text-sm text-fs-8 px-7 py-20 transition-colors duration-300 hover:text-tertiary focus:text-tertiary active:text-primary" data-nav-link>
-          Contact
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
+        <li>
+          <NavLink to="/portfolio" className="text-white text-sm px-7 py-10 transition-colors duration-300 hover:text-tertiary focus:text-primary active:text-primary" data-nav-link>
+            Portfolio
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
   )
 }
 
