8b33d6dece71e819f05912539acb0f0672906cf4
astro0725 <angelica.strong0725@gmail.com>
2024-03-26 13:40:17 -0700

    updated styles

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,22 +3,22 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
     <div className="items-center text-white">
-      <nav className="bg-base p-4 text-gray-300 rounded-lg">
-      <ul className="flex flex-wrap justify-center items-center px-10">
+      <nav className="bg-primary p-4 text-gray-300 rounded-t-lg">
+      <ul className="flex flex-wrap justify-center just items-center px-10">
         <li>
-          <NavLink to="/" className="text-white text-sm px-7 py-10 transition-colors duration-300 hover:text-tertiary focus:text-primary active:text-primary" data-nav-link>
+          <NavLink to="/" className="text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary" data-nav-link>
             About
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="/resume" className="text-white text-sm px-7 py-10 transition-colors duration-300 hover:text-tertiary focus:text-primary active:text-primary" data-nav-link>
+          <NavLink to="/resume" className="text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary" data-nav-link>
             Resume
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="/portfolio" className="text-white text-sm px-7 py-10 transition-colors duration-300 hover:text-tertiary focus:text-primary active:text-primary" data-nav-link>
+          <NavLink to="/portfolio" className="text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary" data-nav-link>
             Portfolio
           </NavLink>
         </li>
