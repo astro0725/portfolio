53adccdf783e0a07da770beb2395da2dd6f054b5
astro0725 <angelica.strong0725@gmail.com>
2024-03-26 12:06:04 -0700

    updated styles

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -3,76 +3,61 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Aside = () => {
   return (
-    <aside className="bg-body rounded-lg p-15 shadow-1 z-10 mb-15 max-h-112 overflow-hidden transition-2">
-      <div className="relative flex justify-start items-center gap-15">
-        <figure className="rounded-full">
-          <img
-            src={"/portrait.png"}
-            alt="Angelica Strong"
-            width="80"
-          />
-        </figure>
+    <aside className="bg-base rounded-lg p-6 shadow-md w-1/4">
+      <div className="flex justify-start items-center gap-4 mb-6">
+        <img
+          className="rounded-full border-2 border-transparent p-1"
+          src={"portrait.PNG"}
+          alt="Angelica Strong"
+          width="80"
+          height="80"
+        />
 
         <div>
-          <h1 className="text-white text-2xl font-medium tracking-tight mb-2.5" title="Angelica Strong">
+          <h1 className="text-white text-xl font-semibold mb-1" title="Angelica Strong">
             Angelica Strong
           </h1>
-
-          <p className="bg-secondary text-white text-xs font-light max-w-max px-3 py-12 rounded-md">
-            Full Stack developer
+          <p className="bg-secondary text-center text-white text-sm font-light px-2 py-1 rounded">
+            Full Stack Developer
           </p>
         </div>
       </div>
 
-      <div>
-        <div className="w-full h-px bg-base my-16"></div>
-
-        <ul className="grid grid-cols-1 gap-16">
-          <li className="min-w-full flex items-center gap-16">
-            <div className="w-48 h-48 rounded-lg text-18">
-              <GiMailbox/>
-            </div>
-
-            <div className="max-w-calc-100-minus-64 w-calc-100-minus-64">
-              <p className="uppercase mb-2 text-highlight">Email</p>
-
+      <div className="my-4">
+        <div className="h-px bg-body my-4"></div>
+        <ul>
+          <li className="flex items-center gap-4 mb-4">
+            <GiMailbox className="text-xl text-primary" />
+            <div>
+              <p className="text-highlight text-xs uppercase">Email</p>
               <a href="mailto:angelica.strong0725@gmail.com" className="text-white">
                 angelica.strong0725@gmail.com
               </a>
             </div>
           </li>
 
-            <li className="min-w-full flex items-center gap-16">
-              <div className="w-48 h-48 rounded-lg text-18">
-                <FaGithub />
-              </div>
-
-              <div className="max-w-calc-100-minus-64 w-calc-100-minus-64">
-                <p className="uppercase mb-2 text-highlight">Github</p>
-
+          <li className="flex items-center gap-4 mb-4">
+            <FaGithub className="text-xl text-primary"/>
+            <div>
+              <p className="text-highlight text-xs uppercase">Github</p>
                 <a href="https://github.com/astro0725" className="text-white">
                   astro0725
                 </a>
-              </div>
-            </li>
+            </div>
+          </li>
 
-            <li className="min-w-full flex items-center gap-16">
-              <div className="w-48 h-48 rounded-lg text-18">
-                <FaLinkedinIn />
-              
-              </div>
-
-              <div className="max-w-calc-100-minus-64 w-calc-100-minus-64">
-                <p className="uppercase mb-2 text-highlight">Linkedin</p>
-
-                <a href="https://www.linkedin.com/in/angelica-strong/" className="text-white">
-                  Angelica Strong
-                </a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </aside>
+          <li className="flex items-center gap-4 mb-4">
+            <FaLinkedinIn className="text-xl text-primary"/>
+            <div>
+              <p className="text-highlight text-xs uppercase">Linkedin</p>
+              <a href="https://www.linkedin.com/in/angelica-strong/" className="text-white">
+                Angelica Strong
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </aside>
   )
 }
 
