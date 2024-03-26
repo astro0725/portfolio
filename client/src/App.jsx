9d29ee408498a21b2626c01c5f9ead950ec239fc
astro0725 <angelica.strong0725@gmail.com>
2024-03-26 12:45:04 -0700

    updated layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
 import './assets/css/tailwind.css'
 
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Aside from "./components/Aside";
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Aside from './components/Aside';
 
 function App() {
   return (
     <>
-      <Aside/>
-      <div className="relative max-w-max m-auto min-w-3/4 w-3/4">
+      <div className='flex'>
+        <Aside/>
         <Navbar/>
         <section style={{
-          scrollBehavior: "smooth"
-        }}><Outlet/></section>
+          scrollBehavior: 'smooth'
+        }}>
+          <Outlet/>
+        </section>
       </div>
     </>
   );
