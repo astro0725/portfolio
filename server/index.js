50ac8a662f5473571150a2be043a46e49d08be7e
astro0725 <angelica.strong0725@gmail.com>
2024-03-26 18:22:06 -0700

    change port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
