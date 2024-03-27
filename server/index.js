a08e4422d2ad71dd2c8f2be61cf5f6c1e4ce18ab
astro0725 <angelica.strong0725@gmail.com>
2024-03-26 17:19:23 -0700

    updated express to serve from correct output build

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('/api/github/:user/repos', async (req, res) => {
   const { user } = req.params;
