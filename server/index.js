2fe30816db909e7c858d183ada58071baece5d6a
astro0725 <angelica.strong0725@gmail.com>
2024-03-26 17:54:24 -0700

    rm * route

--- server/index.js
+++ server/index.js
@@ -61,10 +61,6 @@ app.get('/api/github/:user/:repoName/readme', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
